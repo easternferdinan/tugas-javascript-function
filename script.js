96c97542e6d3c491c66e9ea7831c799b09b7bf3a
easternferdinan <ferdinandrestu03@gmail.com>
2023-01-16 12:59:12 +0700

    Menambahkan konfirmasi keluar dari permainan dan beberapa minor update

--- script.js
+++ script.js
@@ -4,48 +4,56 @@ Permainan ini akan berlangsung selama 5 ronde.
 Pemain yang mendapatkan skor terbanyak akan menang
 SELAMAT BERMAIN.`);
 
-let ronde = 1;
 let scoreP1 = 0;
 let scoreP2 = 0;
+let selesai = true;
 
-while (ronde <= 5) {
-    alert(`Ronde ${ronde}`);
+while (selesai == true) {
+
+    let ronde = 1;
+
+    while (ronde <= 5) {
+        alert(`Ronde ${ronde}`);
+        
+        let player1;
+        let player2;
+        let isValid;
     
-    let player1;
-    let player2;
-    let isValid;
-
-    // Terus menerus meminta input dari user sampai input yang diberikan valid
-    do {
-        player1 = prompt("( Player 1 ) Masukan angka tebakan anda:");
-        player2 = prompt("( Player 2 ) Masukan angka tebakan anda:");
-        isValid = validator(player1, player2);
-    } while (isValid == false);
-
-    let angka = randomNum();
-
-    if (player1 == angka){
-        alert(`Tebakan Player 1 benar. \nAngka: ${angka}`);
-        scoreP1++;
-    } else if (player2 == angka) {
-        alert(`Tebakan Player 2 benar. \nAngka: ${angka}`);
-        scoreP2++;
-    } else {
-        alert("Tidak ada tebakan yang benar.");
+        // Terus menerus meminta input dari user sampai input yang diberikan valid
+        do {
+            player1 = prompt("( Player 1 ) Masukan angka tebakan anda:");
+            player2 = prompt("( Player 2 ) Masukan angka tebakan anda:");
+            isValid = validator(player1, player2);
+        } while (isValid == false);
+    
+        let angka = randomNum();
+    
+        if (player1 == angka){
+            alert(`Tebakan Player 1 benar. \nAngka: ${angka}`);
+            scoreP1++;
+        } else if (player2 == angka) {
+            alert(`Tebakan Player 2 benar. \nAngka: ${angka}`);
+            scoreP2++;
+        } else {
+            alert("Tidak ada tebakan yang benar.");
+        }
+    
+        alert(`Skor \nPlayer 1: ${scoreP1} \nPlayer 2: ${scoreP2}`);
+    
+        ronde++;
     }
 
-    alert(`Skor \nPlayer 1: ${scoreP1} \nPlayer 2: ${scoreP2}`);
+    if (scoreP1 > scoreP2){
+        alert("Player 1 menang!");
+    } else if (scoreP2 > scoreP1){
+        alert("Player 2 menang!");
+    } else {
+        alert("Seri!");
+    }
 
-    ronde++;
+    selesai = confirm("Apakah anda ingin main lagi?");
 }
 
-if (scoreP1 > scoreP2){
-    alert("Player 1 menang!");
-} else if (scoreP2 > scoreP1){
-    alert("Player 2 menang!");
-} else {
-    alert("Seri!");
-}
 
 // Fungsi untuk memastikan input yang diberikan bukanlah huruf atau angka yang diluar 1-3
 function validator(input1, input2) {
