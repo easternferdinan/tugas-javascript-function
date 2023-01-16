a25275fb695bc9e714970004e4436f6ed3c1a94a
easternferdinan <ferdinandrestu03@gmail.com>
2023-01-16 10:28:24 +0700

    File index.html dan script.js dibuat dan pesan selamat datang ditambahkan di script.js

---
+++ script.js
@@ -0,0 +1,6 @@
+alert
+(`Selamat datang di permainan tebak angka.
+Anda diminta untuk menebak angka 1 - 3.
+Permainan ini akan berlangsung selama 5 ronde.
+Pemain yang mendapatkan skor terbanyak akan menang
+SELAMAT BERMAIN.`);
